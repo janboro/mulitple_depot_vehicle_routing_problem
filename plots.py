b90b90cf95557683b5bcd51a3aa037c1e93174ba
Jan Borowiec <janekborowiec@gmail.com>
2021-12-11 11:05:12 +0100

    Refactored plots

--- plots.py
+++ plots.py
@@ -2,19 +2,50 @@ import matplotlib.pyplot as plt
 
 
 class Plotter:
-    def __init__(self, coordinates, title="MDVRP"):
-        self.coordinates = coordinates
+    def __init__(self, problem_map, title="MDVRP"):
+        self.problem_map = problem_map
+        self.initialize_map(title=title)
+
+    def initialize_map(self, title):
         plt.figure()
         plt.axis("off")
         plt.title(title)
 
+    def plot_depots(self):
+        x = [depo.x for depo in self.problem_map.depots]
+        y = [depo.y for depo in self.problem_map.depots]
+        plt.scatter(
+            x,
+            y,
+            color="black",
+            s=120,
+            zorder=2,
+        )
+
     def plot_vertices(self):
-        plt.scatter(self.coordinates[0], self.coordinates[1], s=60, zorder=2)
+        x = [depo.x for depo in self.problem_map.vertices]
+        y = [depo.y for depo in self.problem_map.vertices]
+        plt.scatter(
+            x,
+            y,
+            color="blue",
+            s=60,
+            zorder=2,
+        )
 
     def plot_edges(self):
-        plt.plot(self.coordinates[0], self.coordinates[1], color="C3", zorder=1, lw=2)
+        x = [depo.x for depo in self.problem_map.vertices]
+        y = [depo.y for depo in self.problem_map.vertices]
+        plt.plot(
+            x,
+            y,
+            color="C3",
+            zorder=1,
+            lw=2,
+        )
 
     def plot_all(self):
+        self.plot_depots()
         self.plot_vertices()
         self.plot_edges()
         self.show_map()
