38494cafa507691a0cbe3929394e34d9fe27b759
Jan Borowiec <janekborowiec@gmail.com>
2022-01-01 12:28:29 +0100

    Finished benchmarking data

--- benchmarking.py
+++ benchmarking.py
@@ -8,7 +8,7 @@ def main():
     for no_of_depots in range(1, 21):
         for no_of_vertices in range(50, 1001, 50):
             for _ in range(3):
-                # print(f"no_of_depots: {no_of_depots}\nearest_neighouro_of_vertices: {no_of_vertices}")
+                print(f"no_of_depots: {no_of_depots}\nno_of_vertices: {no_of_vertices}")
                 data_row = [no_of_depots, no_of_vertices]
                 # Generating map
                 mdvrp = MDVRP(no_of_vertices=no_of_vertices, no_of_depots=no_of_depots, map_size=1000)
@@ -16,15 +16,12 @@ def main():
                 # Assigning vertices to depots
                 assigning_time = get_execution_time(function=mdvrp.solver.assign_vertices_to_depot)
                 data_row.append(assigning_time)
-                # print(f"assigning_time: {assigning_time}")
 
                 # Nearest Neighbour
                 nearest_neighbour_execution_time = get_execution_time(function=mdvrp.solver.solve_nearest_neighbour)
                 nearest_neighbour_distances = mdvrp.get_distances()
                 data_row.append(nearest_neighbour_execution_time)
                 data_row.append(nearest_neighbour_distances)
-                # print(f"nearest_neighbour_execution_time: {nearest_neighbour_execution_time}")
-                # print(f"nearest_neighbour_distances: {nearest_neighbour_distances}")
 
                 # Nearest Neighbour min max
                 if no_of_depots > 1:
@@ -37,16 +34,14 @@ def main():
                     nearest_neighour_min_max_distances = None
                 data_row.append(nearest_neighour_min_max_execution_time)
                 data_row.append(nearest_neighour_min_max_distances)
-                # print(f"nearest_neighour_min_max_time: {nearest_neighour_min_max_time}")
-                # print(f"nearest_neighour_min_max_distances: {nearest_neighour_min_max_distances}")
+
                 mdvrp.solver.reset_solution()
+
                 # Nearest Insertion
                 nearest_insertion_execution_time = get_execution_time(function=mdvrp.solver.solve_nearest_insertion)
                 nearest_insertion_distances = mdvrp.get_distances()
                 data_row.append(nearest_insertion_execution_time)
                 data_row.append(nearest_insertion_distances)
-                # print(f"nearest_insertion_execution_time: {nearest_insertion_execution_time}")
-                # print(f"nearest_insertion_distances: {nearest_insertion_distances}")
 
                 # Nearest Insertion min max
                 if no_of_depots > 1:
@@ -59,9 +54,6 @@ def main():
                     nearest_insertion_min_max_distances = None
                 data_row.append(nearest_insertion_min_max_execution_time)
                 data_row.append(nearest_insertion_min_max_distances)
-                # print(f"nearest_insertion_min_maxexecution__time: {nearest_insertion_min_max_texecution_ime}")
-                # print(f"nearest_insertion_min_max_distances: {nearest_insertion_min_max_distances}")
-                print("Finished loop\n")
 
                 current_directory = os.path.dirname(os.path.realpath(__file__))
                 csv_file_path = os.path.normpath(os.path.join(current_directory, "MDVRP_benchmark.csv"))
